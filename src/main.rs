67c8f10f0bf21c173bf1e888f56e0831ed8bb7ed
Pierre-Francois Loos <pierrefrancois.loos@gmail.com>
2021-11-14 22:46:33 +0100

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let message = String::from("Hello qrust! Let's do quantum chemistry with Rust!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
 }
